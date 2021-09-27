3ee8baa3eeb7d063c21f79dd4986a9e7c06220fb
techweed <josephantonykj006@gmail.com>
2021-09-28 02:39:57 +0530

    changed home route

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,20 @@
-import { lazy } from 'react';
-const Home = lazy(() => import('./pages/home/home'));
-const Weather = lazy(() => import('./pages/weather/weather'));
+import { lazy } from "react";
+const Home = lazy(() => import("./pages/home/home"));
+const Weather = lazy(() => import("./pages/weather/weather"));
 
 const routes = [
-	{
-		exact: true,
-		name: 'Home',
-		path: '/home',
-		component: Home,
-	},
-	{
-		exact: true,
-		name: 'Weather',
-		path: '/weather',
-		component: Weather,
-	},
+  {
+    exact: true,
+    name: "Home",
+    path: "/",
+    component: Home,
+  },
+  {
+    exact: true,
+    name: "Weather",
+    path: "/weather",
+    component: Weather,
+  },
 ];
 
 export default routes;
